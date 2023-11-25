742102e21973ae958e89920259cc0d3fc598d7fa
mariyemel <mariyem.elm02@gmail.com>
2023-11-25 17:54:23 +0100

    js

---
+++ jss/menu.js
@@ -0,0 +1,7 @@
+/*Ce code permet d'afficher le responsive menu*/
+var menu_toggle = document.querySelector('header .menu-toggle');
+var menu = document.querySelector('header .menu');
+menu_toggle.onclick = function () {
+    menu_toggle.classList.toggle('active');
+    menu.classList.toggle('responsive');
+}

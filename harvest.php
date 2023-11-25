18f291be35f2656b862549d334c24198e8b98875
mariyemel <mariyem.elm02@gmail.com>
2023-11-25 21:41:13 +0100

    code with doc

--- harvest.php
+++ harvest.php
@@ -1,10 +1,13 @@
 <?php
+// Include necessary files for database connection and configuration
 include './connection/db_connection.php';
 include('Config.php');
-// Obtenez les plantes sélectionnées
+
+// Get the selected plants from the database
 $selection_result = getSelectedPlants();
 $selected_plants = array();
 
+// Fetch plant details and create an associative array
 while ($row = $selection_result->fetch_assoc()) {
     $selected_plants[] = array(
         'label' => $row['plant_name'],
@@ -13,38 +16,53 @@ while ($row = $selection_result->fetch_assoc()) {
 }
 ?>
 <?php
+// Start or resume a session
 session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
+    <!-- Set character set and viewport for responsiveness -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Page title -->
     <title>Harvest guide</title>
+
+    <!-- Include Font Awesome for icons -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Stylesheets for page styling -->
     <link rel="stylesheet" href="./style/styleMultitag.css">
     <link rel="stylesheet" href="./style/header.css">
     <link rel="stylesheet" href="./style/harvest.css">
     <link rel="stylesheet" href="./style/menuServices.css">
     <link rel="stylesheet" href="./style/footer.css">
+
+    <!-- Include JavaScript for multiselect tag and other functionalities -->
     <script src="./jss/multitag.js"></script>
 
 </head>
 
 <body>
+    <!-- Header section -->
     <header>
         <div class="logo">
             <img src="./img/logo2.png" alt="logo">
         </div>
+
         <!-- menu responsive -->
         <div class="menu-toggle"></div>
 
+        <!-- Navigation menu -->
         <ul class="menu">
             <li><a href="./home.php">Home</a></li>
             <li><a href="./about.php">About Us</a></li>
             <li><a href="./services.php">Services</a></li>
             <li><a href="./contact.php">Contact</a></li>
+
+            <!-- Display "Déconnexion" (Logout) if a user is logged in, otherwise show "Sign-In" -->
             <?php if (isset($_SESSION['id'])) : ?>
                 <li><a href="logout.php">Déconnexion</a></li>
             <?php else : ?>
@@ -52,26 +70,32 @@ session_start();
             <?php endif; ?>
         </ul>
     </header>
+
+    <!-- Plant selection form -->
     <div class="plant-selection">
         <h3>Selected plants:</h3>
+        <!-- Form for selecting multiple plants -->
         <form name="plant_selection" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
             <select name="plants[]" id="plants" multiple>
+                <!-- Populate dropdown with selected plant options -->
                 <?php
                 foreach ($selected_plants as $plant) {
                     echo "<option value=\"" . htmlspecialchars($plant['value']) . "\">" . htmlspecialchars($plant['label']) . "</option>";
                 }
                 ?>
             </select>
+            <!-- Submit button for plant selection -->
             <input type="submit" name="submit_plants" value="Submit plant selection">
         </form>
     </div>
 
+    <!-- Display details of selected plants -->
     <div class="resultat">
         <?php
         if (isset($_POST['submit_plants'])) {
             $selectedValues = $_POST['plants'];
 
-            // La requête préparée commence ici
+            // Prepared SQL query to retrieve specific information about selected plants
             $placeholders = implode(',', array_fill(0, count($selectedValues), '?'));
             $sql = "SELECT p.plant_id, plant_name, harvest_ready, harvest_instruct, i.image_url AS harvest_image
             FROM plants p
@@ -79,12 +103,13 @@ session_start();
             LEFT JOIN plants_id pid ON p.plant_id = pid.plant_id
             WHERE p.plant_id IN ($placeholders) AND p.plant_id = pid.plant_id";
 
-
+            // Prepare and execute the SQL statement
             if ($stmt = mysqli_prepare($conn, $sql)) {
                 mysqli_stmt_bind_param($stmt, str_repeat('i', count($selectedValues)), ...$selectedValues);
                 mysqli_stmt_execute($stmt);
                 $result = mysqli_stmt_get_result($stmt);
 
+                // Display details of each selected plant
                 if ($result && mysqli_num_rows($result) > 0) {
                     while ($row = mysqli_fetch_assoc($result)) {
                         echo "<h3>Selected plant: " . htmlspecialchars($row['plant_name']) . "</h3>";
@@ -108,8 +133,9 @@ session_start();
         }
         ?>
     </div>
-    <div class="default-info">
 
+    <!-- Default information sections -->
+    <div class="default-info">
         <h2>Harvesting Plants with Precision:</h2>
         <p>
             Harvesting marks the culmination of careful cultivation, where the fruits of labor are gathered at their peak. Timing is
@@ -119,6 +145,8 @@ session_start();
         </p>
         <h2>More Services :</h2>
     </div>
+
+    <!-- Section for displaying other gardening services -->
     <section id="our-services">
         <div class="liste-photos">
             <div class="service-img">
@@ -159,14 +187,19 @@ session_start();
             </div>
         </div>
     </section>
-    <!-- footer -->
+
+    <!-- Footer section -->
     <footer>
         <div>
             <i class="fa fa-leaf" aria-hidden="true"></i>
             <p>Copyright HeyAgri</p>
         </div>
     </footer>
+
+    <!-- Include script for responsive menu -->
     <script src="./jss/menu.js"></script>
+
+    <!-- Initialize the Multiselect Tag functionality -->
     <script>
         new MultiSelectTag('plants', {
             rounded: true,
@@ -176,6 +209,8 @@ session_start();
                 console.log(values)
             }
         })
+
+        // Prevent caching of pages in the browser's history
         if (window.history.replaceState) {
             window.history.replaceState(null, null, window.location.href);
         }
